52f6fa19cc4ad0cada71afcce1f6344ba729cf56
dejan-crocoder <dejan@crocoder.dev>
2023-07-21 14:31:13 +0200

    export schemas out of module

--- packages/schemas/extract/src/index.ts
+++ packages/schemas/extract/src/index.ts
@@ -2,3 +2,13 @@ export { repositories } from './repositories';
 export type { Repository, NewRepository } from './repositories';
 export { namespaces } from './namespaces';
 export type { Namespace, NewNamespace } from './namespaces';
+export { mergeRequests } from './mergeRequests';
+export type { MergeRequest, NewMergeRequest } from './mergeRequests'
+export { mergeRequestCommits } from './mergeRequestCommits';
+export type { MergeRequestCommit, NewMergeRequestCommit } from './mergeRequestCommits';
+export { mergeRequestDiffs } from './mergeRequestDiffs';
+export type { MergeRequestDiff, NewMergeRequestDiff } from './mergeRequestDiffs';
+export { members } from './members';
+export type { Member, NewMember } from './members';
+export { projectsToMembers } from './projectsToMembers';
+export type { ProjectToMember, NewProjectToMember } from './projectsToMembers';
