eab2e862fba9d0a94dda16bb1bb0f4f3f1f421fb
David Abram <david@crocoder.dev>
2023-07-25 11:52:39 +0200

    merge request schema

--- packages/schemas/extract/src/index.ts
+++ packages/schemas/extract/src/index.ts
@@ -2,3 +2,5 @@ export { repositories } from './repositories';
 export type { Repository, NewRepository } from './repositories';
 export { namespaces } from './namespaces';
 export type { Namespace, NewNamespace } from './namespaces';
+export { mergeRequests } from './merge-requests';
+export type { MergeRequest, NewMergeRequest } from './merge-requests';
