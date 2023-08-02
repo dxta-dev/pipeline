b832c1e1b1f09e65c77ff6eed8308bb1433ea2da
David Abram <david@crocoder.dev>
2023-08-02 14:35:16 +0200

    removed rules

--- apps/extract-stack/src/stack.ts
+++ apps/extract-stack/src/stack.ts
@@ -1,15 +1,11 @@
 import { Api, EventBus } from "sst/constructs";
 import type { StackContext } from "sst/constructs";
-import { extractRepositoryEvent } from "./events";
 
 export function ExtractStack({ stack }: StackContext) {
   const bus = new EventBus(stack, "ExtractBus", {
     defaults: {
       retries: 10,
     },
-    rules: {
-
-    }
   });
 
   const api = new Api(stack, "ExtractApi", {
@@ -19,8 +15,7 @@ export function ExtractStack({ stack }: StackContext) {
       },
     },
     routes: {
-      "POST /gitlab": "packages/functions/list.handler",
-      "POST /github": "packages/functions/list.handler",
+      "POST /gitlab": "extract-repository.handler",
     },
   });
 
