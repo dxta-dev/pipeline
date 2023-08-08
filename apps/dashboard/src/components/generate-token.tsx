93c1f3bc1273c8b52bfaf7690633cc6a75743e75
David Abram <david@crocoder.dev>
2023-08-08 13:31:37 +0200

    fix???

--- apps/dashboard/src/components/generate-token.tsx
+++ apps/dashboard/src/components/generate-token.tsx
@@ -11,7 +11,7 @@ export function GenerateToken() {
 
 
   return (
-    <button onClick={() => handleClick()}>
+    <button onClick={() => { handleClick() }}>
       Generate Token
     </button>
   );
