71d4e96f7b67d8b1ee7b8d7ad9c8f35799645dd7
David Abram <david@crocoder.dev>
2023-08-10 12:27:56 +0200

    DELETED!

--- apps/extract-stack/src/t.ts
+++
@@ -1,6 +0,0 @@
-import { EventHandler } from "sst/node/event-bus";
-import { extractRepositoryEvent } from "./events";
-
-export const handler = EventHandler(Todo.Events.Created, async (evt) => {
-  console.log("Todo created", evt);
-});
