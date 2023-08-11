ca89fdfa06e7a03c52e5f3f86cec189aa1cc3faf
David Abram <david@crocoder.dev>
2023-08-11 12:04:36 +0200

    batch messages

--- apps/extract-stack/src/create-message.ts
+++ apps/extract-stack/src/create-message.ts
@@ -11,11 +11,43 @@ content: Content<Shape>,
 metadata: MetadataShape
 ) => Promise<void>;
 
+type BatchSend<Shape extends ZodRawShape, MetadataShape extends ZodRawShape> = (
+  content: Content<Shape>[],
+  metadata: MetadataShape
+) => Promise<void>;
+
 type MessageProps<QueueUrl extends string, Shape extends ZodRawShape, MetadataShape extends ZodRawShape> = {
   queueUrl: QueueUrl;
   contentShape: Content<Shape>;
   metadataShape: MetadataShape;
 };
+
+export function createBatchMessage<QueueUrl extends string, Shape extends ZodRawShape, MetadataShape extends ZodRawShape>({
+  queueUrl,
+  contentShape,
+  metadataShape,
+}: MessageProps<QueueUrl, Shape, MetadataShape>) {
+
+  const messageSchema = z.object({
+    content: z.object(contentShape),
+    metadata: z.object(metadataShape),
+  });
+
+  const send: BatchSend<Shape, MetadataShape> = async (content, metadata) => {
+    console.log("sending", { content, metadata });
+    await sqs.sendMessageBatch({
+      QueueUrl: queueUrl,
+      Entries: content.map((c) => ({
+        Id: ,
+        MessageBody: JSON.stringify(messageSchema.parse({ content: c, metadata })),
+      })),
+    }).promise();
+  }
+
+  return {
+    send
+  }
+}
 
 export function createMessage<QueueUrl extends string, Shape extends ZodRawShape, MetadataShape extends ZodRawShape>({
   queueUrl,
