98643ee0b765b03e5e3e9b5bc501e27ae9121597
dejan-crocoder <dejan@crocoder.dev>
2023-08-17 11:12:36 +0200

    coerce dates in zod db schemas

--- packages/schemas/extract/src/namespaces.ts
+++ packages/schemas/extract/src/namespaces.ts
@@ -2,6 +2,7 @@ import type { InferModel } from 'drizzle-orm';
 import { sql } from 'drizzle-orm';
 import { sqliteTable, text, integer, uniqueIndex } from 'drizzle-orm/sqlite-core';
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
+import { z } from 'zod';
 
 export const namespaces = sqliteTable('namespaces', {
   id: integer('id').primaryKey(),
@@ -15,5 +16,11 @@ export const namespaces = sqliteTable('namespaces', {
 
 export type Namespace = InferModel<typeof namespaces>;
 export type NewNamespace = InferModel<typeof namespaces, 'insert'>;
-export const NewNamespaceSchema = createInsertSchema(namespaces);
-export const NamespaceSchema = createSelectSchema(namespaces);
+export const NewNamespaceSchema = createInsertSchema(namespaces, {
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date(),
+});
+export const NamespaceSchema = createSelectSchema(namespaces, {
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date(),
+});
