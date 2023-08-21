a38f856b7b1fdca24ba4cc4f3dcabe826c511f3b
dejan-crocoder <dejan@crocoder.dev>
2023-08-21 17:23:02 +0200

    implement get function

--- packages/functions/extract/src/get-merge-request-diffs.ts
+++ packages/functions/extract/src/get-merge-request-diffs.ts
@@ -1,8 +1,9 @@
 import type { MergeRequestDiff } from "@acme/extract-schema";
 import type { ExtractFunction, Entities } from "./config";
 import type { Pagination, SourceControl } from "@acme/source-control";
-// import type { LibSQLDatabase } from "drizzle-orm/libsql";
-// import type { BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
+import { eq } from "drizzle-orm";
+import type { LibSQLDatabase } from "drizzle-orm/libsql";
+import type { BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
 
 export type GetMergeRequestDiffsInput = {
   repositoryId: number;
@@ -26,5 +27,47 @@ export const getMergeRequestsDiffs: GetMergeRequestDiffsFunction = async (
   { mergeRequestId, namespaceId, repositoryId, page, perPage },
   { db, entities, integrations }
 ) => {
-  throw new Error("not yet implemented");
+  const namespace = await db.select().from(entities.namespaces).where(eq(entities.namespaces.id, namespaceId)).get();
+  if (!namespace) throw new Error(`Invalid namespace: ${namespaceId}`);
+
+  const repository = await db.select().from(entities.repositories).where(eq(entities.repositories.id, repositoryId)).get();
+  if (!repository) throw new Error(`Invalid repository: ${repositoryId}`);
+
+  const mergeRequest = await db.select().from(entities.mergeRequests).where(eq(entities.mergeRequests.id, mergeRequestId)).get();
+  if (!mergeRequest) throw new Error(`Invalid mergeRequest: ${mergeRequestId}`);
+
+  if (!integrations.sourceControl) {
+    throw new Error("Source control integration not configured");
+  }
+
+  const { mergeRequestDiffs, pagination } = await integrations.sourceControl.fetchMergeRequestDiffs(repository, namespace, mergeRequest, page, perPage);
+
+  console.log('mergeRequestDiffs', ...mergeRequestDiffs);
+
+  const insertedMergeRequestDiffs = await (db as (LibSQLDatabase & BetterSQLite3Database)).transaction(async (tx) => {
+    return Promise.all(mergeRequestDiffs.map(mergeRequestDiff =>
+      tx.insert(entities.mergeRequestDiffs).values(mergeRequestDiff)
+        .onConflictDoUpdate({
+          target: [entities.mergeRequestDiffs.mergeRequestId, entities.mergeRequestDiffs.newPath],
+          set: {
+            diff: mergeRequestDiff.diff,
+            newPath: mergeRequestDiff.newPath,
+            oldPath: mergeRequestDiff.oldPath,
+            aMode: mergeRequestDiff.aMode,
+            bMode: mergeRequestDiff.bMode,
+            newFile: mergeRequestDiff.newFile,
+            renamedFile: mergeRequestDiff.renamedFile,
+            deletedFile: mergeRequestDiff.deletedFile,
+            _updatedAt: new Date(),
+          }
+        })
+        .returning()
+        .get()
+    ));
+  });
+
+  return {
+    mergeRequestDiffs: insertedMergeRequestDiffs,
+    paginationInfo: pagination
+  }
 }
