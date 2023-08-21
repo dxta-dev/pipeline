43773f14256233405f55969d6729f918f423d063
dejan-crocoder <dejan@crocoder.dev>
2023-08-21 14:13:00 +0200

    add db entities as input to fetch mr diffs

--- packages/integrations/source-control/src/source-control.ts
+++ packages/integrations/source-control/src/source-control.ts
@@ -1,4 +1,5 @@
 import type { NewRepository, NewNamespace, NewMergeRequest, NewMember, NewMergeRequestDiff } from "@acme/extract-schema";
+import type { Repository, Namespace, MergeRequest } from "@acme/extract-schema";
 
 export type Pagination = {
   page: number;
@@ -15,5 +16,5 @@ export interface SourceControl {
   fetchRepository(externalRepositoryId: number, namespaceName: string, repositoryName: string): Promise<{ repository: NewRepository, namespace?: NewNamespace }>;
   fetchMembers(externalRepositoryId: number, namespaceName: string, repositoryName: string, page?: number, perPage?: number): Promise<{ members: NewMember[], pagination: Pagination }>;
   fetchMergeRequests(externalRepositoryId: number, namespaceName: string, repositoryName: string, repositoryId: number, creationPeriod?: TimePeriod, page?: number, perPage?: number): Promise<{ mergeRequests: NewMergeRequest[], pagination: Pagination }>;
-  fetchMergeRequestDiffs(externalRepositoryId: number, namespaceName: string, repositoryName: string, mergeRequestNumber: number, page?: number, perPage?: number): Promise<{ mergeRequestDiffs: NewMergeRequestDiff[], pagination: Pagination }>;
+  fetchMergeRequestDiffs(repository: Repository, namespace: Namespace, mergeRequest: MergeRequest, page?: number, perPage?: number): Promise<{ mergeRequestDiffs: NewMergeRequestDiff[], pagination: Pagination }>;
 }
