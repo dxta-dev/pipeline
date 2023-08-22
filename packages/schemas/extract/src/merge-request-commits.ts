707f9e70b64e40c6a61bbcde86a477766650f251
Ante-Koceic <97022082+Ante-Koceic@users.noreply.github.com>
2023-08-22 12:19:52 +0200

    fix:merge-request-commits-schema (#114)
    - changes the type of external_id
    - generated new .sql file and snapshot

--- packages/schemas/extract/src/merge-request-commits.ts
+++ packages/schemas/extract/src/merge-request-commits.ts
@@ -1,12 +1,14 @@
 import type { InferModel } from 'drizzle-orm';
 import { sql } from 'drizzle-orm';
 import { sqliteTable, text, integer, uniqueIndex } from 'drizzle-orm/sqlite-core';
+import { createInsertSchema } from 'drizzle-zod';
+import { z } from 'zod';
 
 export const mergeRequestCommits = sqliteTable('merge_request_commits', {
   id: integer('id').primaryKey(),
   /* Gitlab -> iid */
   mergeRequestId: integer('merge_request_id').notNull(),
-  externalId: integer('external_id').notNull(),
+  externalId: text('external_id').notNull(),
   createdAt: integer('created_at', { mode: 'timestamp_ms' }).notNull(),
   authoredDate: integer('authored_date', { mode: 'timestamp_ms' }).notNull(),
   committedDate: integer('committed_date', { mode: 'timestamp_ms' }).notNull(),
@@ -24,3 +26,17 @@ export const mergeRequestCommits = sqliteTable('merge_request_commits', {
 
 export type MergeRequestCommit = InferModel<typeof mergeRequestCommits>;
 export type NewMergeRequestCommit = InferModel<typeof mergeRequestCommits, 'insert'>;
+export const MergeRequestCommitSchema = createInsertSchema(mergeRequestCommits, {
+  createdAt: z.coerce.date(),
+  authoredDate: z.coerce.date(),
+  committedDate: z.coerce.date(),
+  _createdAt: z.coerce.date(),
+  _updatedAt: z.coerce.date(),
+});
+export const NewMergeRequestCommitSchema = createInsertSchema(mergeRequestCommits, {
+  createdAt: z.coerce.date(),
+  authoredDate: z.coerce.date(),
+  committedDate: z.coerce.date(),
+  _createdAt: z.coerce.date(),
+  _updatedAt: z.coerce.date(),
+});
