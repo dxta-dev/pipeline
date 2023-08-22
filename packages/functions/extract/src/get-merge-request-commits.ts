a97a779588ee0a118014a59affae797b1b9a9684
Ante Koceić <ante@crocoder.dev>
2023-08-22 15:51:16 +0200

    Created get-merge-request-commits file

---
+++ packages/functions/extract/src/get-merge-request-commits.ts
@@ -0,0 +1,54 @@
+import type { SourceControl } from "@acme/source-control";
+import type { Entities, ExtractFunction } from "./config"
+import { type MergeRequestCommit } from "@acme/extract-schema";
+import type { LibSQLDatabase } from "drizzle-orm/libsql";
+import type{ BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
+
+export type GetMergeRequestCommitsInput = {
+  externalRepositoryId: number;
+  namespaceName: string;
+  repositoryName: string;
+  mergerequestIId: number;
+};
+
+export type GetMergeRequestCommitsOutput = {
+  mergeRequestCommits: MergeRequestCommit[];
+};
+
+export type GetMergeRequestCommitsSourceControl = Pick<SourceControl, "fetchMergeRequestCommits">;
+export type GetMergeRequestCommitsEntities = Pick<Entities, "mergeRequestCommits">;
+
+export type GetMergeRequestCommitsFunction = ExtractFunction<GetMergeRequestCommitsInput, GetMergeRequestCommitsOutput, GetMergeRequestCommitsSourceControl, GetMergeRequestCommitsEntities>
+
+export const getMergeRequestCommits: GetMergeRequestCommitsFunction = async (
+  { externalRepositoryId, namespaceName, repositoryName, mergerequestIId },
+  { integrations, db, entities },
+) => {
+
+  if(!integrations.sourceControl) {
+    throw new Error("Source control integration not configured");
+  }
+
+  const { mergeRequestCommits } = await integrations.sourceControl.fetchMergeRequestCommits(externalRepositoryId, namespaceName, repositoryName, mergerequestIId, {});
+
+  const insertedMergeRequestCommits = await (db as (LibSQLDatabase & BetterSQLite3Database)).transaction(async (tx) => {
+    return Promise.all(mergeRequestCommits.map(mergeRequestCommit => 
+      tx.insert(entities.mergeRequestCommits).values(mergeRequestCommit)
+        .onConflictDoUpdate({ 
+          target: entities.mergeRequestCommits.externalId,
+          set: { 
+            createdAt: mergeRequestCommit.createdAt,
+            committedDate: mergeRequestCommit.committedDate,
+            title: mergeRequestCommit.title,
+            message: mergeRequestCommit.message,
+            _updatedAt: new Date(),
+          } })
+        .returning()
+        .get()
+      ));
+  });
+
+  return {
+    mergeRequestCommits: insertedMergeRequestCommits,
+  }
+};
