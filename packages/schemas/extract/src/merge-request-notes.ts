42c7636b0e77e475a1c5ce82bfdf207fd851095d
dejan-crocoder <dejan@crocoder.dev>
2023-08-25 15:59:26 +0200

    add mr id with comment

--- packages/schemas/extract/src/merge-request-notes.ts
+++ packages/schemas/extract/src/merge-request-notes.ts
@@ -5,7 +5,7 @@ import { sqliteTable, text, integer, uniqueIndex } from 'drizzle-orm/sqlite-core
 export const mergeRequestNotes = sqliteTable('merge_request_notes', {
   id: integer('id').primaryKey(),
   externalId: integer('external_id').notNull(),
-  // mergeRequestId: integer('merge_request_id').notNull(),
+  mergeRequestId: integer('merge_request_id').notNull(), // internalId not mergeRequestId
   createdAt: integer('created_at', { mode: 'timestamp_ms' }).notNull(),
   updatedAt: integer('updated_at', { mode: 'timestamp_ms' }).notNull(),
   authorUsername: integer('author_username').notNull(),
