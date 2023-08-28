da880ecd6eb8ca6f511e1c305dde205f7855ef4f
Dejan Tot <139872861+dejan-crocoder@users.noreply.github.com>
2023-08-28 11:46:58 +0200

    [TOOL-53] Merge Request Note Extract Schema (#137)
    * add schema draft
    
    * add mr id with comment
    
    * export schema
    
    * remove body and sys columns
    
    * add migration
    
    ---------
    
    Co-authored-by: David Abram <david@crocoder.dev>

---
+++ packages/schemas/extract/src/merge-request-notes.ts
@@ -0,0 +1,20 @@
+import type { InferModel } from 'drizzle-orm';
+import { sql } from 'drizzle-orm';
+import { sqliteTable, integer, uniqueIndex } from 'drizzle-orm/sqlite-core';
+
+export const mergeRequestNotes = sqliteTable('merge_request_notes', {
+  id: integer('id').primaryKey(),
+  externalId: integer('external_id').notNull(),
+  mergeRequestId: integer('merge_request_id').notNull(), // internalId not mergeRequestId
+  createdAt: integer('created_at', { mode: 'timestamp_ms' }).notNull(),
+  updatedAt: integer('updated_at', { mode: 'timestamp_ms' }).notNull(),
+  authorUsername: integer('author_username').notNull(),
+  authorExternalId: integer('author_external_id').notNull(),
+  _createdAt: integer('__created_at', { mode: 'timestamp_ms' }).default(sql`CURRENT_TIMESTAMP`),
+  _updatedAt: integer('__updated_at', { mode: 'timestamp_ms' }).default(sql`CURRENT_TIMESTAMP`),
+}, (notes)=>({
+  uniqueExternalId: uniqueIndex('merge_request_notes_external_id_idx').on(notes.externalId),
+}));
+
+export type MergeRequestNote = InferModel<typeof mergeRequestNotes>;
+export type NewMergeRequestNote = InferModel<typeof mergeRequestNotes, 'insert'>;
