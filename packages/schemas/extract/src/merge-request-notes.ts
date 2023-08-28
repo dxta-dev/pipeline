d974a712a368a495946f862ee618245565f96314
Dejan Tot <139872861+dejan-crocoder@users.noreply.github.com>
2023-08-28 13:58:50 +0200

    Fix: authorUsername should be a string (#138)
    * change username type
    
    * regenerate migrations

--- packages/schemas/extract/src/merge-request-notes.ts
+++ packages/schemas/extract/src/merge-request-notes.ts
@@ -1,6 +1,6 @@
 import type { InferModel } from 'drizzle-orm';
 import { sql } from 'drizzle-orm';
-import { sqliteTable, integer, uniqueIndex } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, integer, uniqueIndex, text } from 'drizzle-orm/sqlite-core';
 
 export const mergeRequestNotes = sqliteTable('merge_request_notes', {
   id: integer('id').primaryKey(),
@@ -8,7 +8,7 @@ export const mergeRequestNotes = sqliteTable('merge_request_notes', {
   mergeRequestId: integer('merge_request_id').notNull(), // internalId not mergeRequestId
   createdAt: integer('created_at', { mode: 'timestamp_ms' }).notNull(),
   updatedAt: integer('updated_at', { mode: 'timestamp_ms' }).notNull(),
-  authorUsername: integer('author_username').notNull(),
+  authorUsername: text('author_username').notNull(),
   authorExternalId: integer('author_external_id').notNull(),
   _createdAt: integer('__created_at', { mode: 'timestamp_ms' }).default(sql`CURRENT_TIMESTAMP`),
   _updatedAt: integer('__updated_at', { mode: 'timestamp_ms' }).default(sql`CURRENT_TIMESTAMP`),
