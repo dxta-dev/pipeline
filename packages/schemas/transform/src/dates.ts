82338069811f093df0558b2396ea4d6cd81295a0
dejan-crocoder <dejan@crocoder.dev>
2023-09-05 14:43:21 +0200

    date data shouldnt be null

--- packages/schemas/transform/src/dates.ts
+++ packages/schemas/transform/src/dates.ts
@@ -3,10 +3,10 @@ import { sqliteTable, integer, uniqueIndex } from 'drizzle-orm/sqlite-core';
 
 export const dates = sqliteTable('dates', {
   id: integer('id').primaryKey(),
-  day: integer('day'),
-  week: integer('week'),
-  month: integer('month'),
-  year: integer('year'),
+  day: integer('day').notNull(),
+  week: integer('week').notNull(),
+  month: integer('month').notNull(),
+  year: integer('year').notNull(),
 }, (dates) => ({
   uniqueDateIndex: uniqueIndex('dates_day_week_month_year_idx').on(dates.day, dates.week, dates.month, dates.year)
 }));
