74932eadca755590f2d788f7740df8d313c1d2ef
dejan-crocoder <dejan@crocoder.dev>
2023-09-05 14:44:15 +0200

    add meta cols to dates table

--- packages/schemas/transform/src/dates.ts
+++ packages/schemas/transform/src/dates.ts
@@ -1,4 +1,5 @@
 import type { InferModel } from 'drizzle-orm';
+import { sql } from 'drizzle-orm';
 import { sqliteTable, integer, uniqueIndex } from 'drizzle-orm/sqlite-core';
 
 export const dates = sqliteTable('dates', {
@@ -7,6 +8,8 @@ export const dates = sqliteTable('dates', {
   week: integer('week').notNull(),
   month: integer('month').notNull(),
   year: integer('year').notNull(),
+  _createdAt: integer('__created_at', { mode: 'timestamp_ms' }).default(sql`CURRENT_TIMESTAMP`),
+  _updatedAt: integer('__updated_at', { mode: 'timestamp_ms' }).default(sql`CURRENT_TIMESTAMP`),
 }, (dates) => ({
   uniqueDateIndex: uniqueIndex('dates_day_week_month_year_idx').on(dates.day, dates.week, dates.month, dates.year)
 }));
