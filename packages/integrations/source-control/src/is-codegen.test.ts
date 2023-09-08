c23f282e10eec4e146b6f685d23abd8a57e191e8
dejan-crocoder <dejan@crocoder.dev>
2023-09-08 14:58:25 +0200

    test matching index files, and minified js and css files

--- packages/integrations/source-control/src/is-codegen.test.ts
+++ packages/integrations/source-control/src/is-codegen.test.ts
@@ -2,11 +2,25 @@ import { describe, expect, it } from '@jest/globals';
 import { isCodeGen } from './is-codegen';
 
 describe("is-codegen", () =>
-  describe('js-codegen', () =>
-    describe('isCodeGen', () => {
+  describe('function isCodeGen', () => {
+    describe('# JavaScript Projects', () => {      
+      
+      it('should not match project files', ()=> {
+        expect(isCodeGen('index.js')).toStrictEqual(false);
+        expect(isCodeGen('./index.js')).toStrictEqual(false);
+        expect(isCodeGen('index.ts')).toStrictEqual(false);
+        expect(isCodeGen('./index.ts')).toStrictEqual(false);
+        expect(isCodeGen('packages/a-module/index.ts')).toStrictEqual(false);
+        expect(isCodeGen('./packages/a-module/index.ts')).toStrictEqual(false);
+      })
+
       it('should match the root package-lock.json file', () => {
         expect(isCodeGen('package-lock.json')).toStrictEqual(true);
         expect(isCodeGen('./package-lock.json')).toStrictEqual(true);
+
+        expect(isCodeGen('package-lockajson')).toStrictEqual(false);
+        expect(isCodeGen('package-lockaajson')).toStrictEqual(false);
+        expect(isCodeGen('package-lock/file/a.json')).toStrictEqual(false);
       })
 
       it('should match nested package-lock.json files', () => {
@@ -14,6 +28,14 @@ describe("is-codegen", () =>
         expect(isCodeGen('./packages/a-module/package-lock.json')).toStrictEqual(true);
       })
 
+      it('should match minified files', ()=> {
+        expect(isCodeGen('dist/out.min.js')).toStrictEqual(true);
+        expect(isCodeGen('./dist/out.min.js')).toStrictEqual(true);
+        
+        expect(isCodeGen('dist/out.min.css')).toStrictEqual(true);
+        expect(isCodeGen('./dist/out.min.css')).toStrictEqual(true);        
+      })
+
     })
-  )
+  })
 )
