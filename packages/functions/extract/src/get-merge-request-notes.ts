818c69a9626f42f46c2d0bb53184aeba9505b4e4
David Abram <david@crocoder.dev>
2023-09-11 15:38:06 +0200

    removed references to specific db

--- packages/functions/extract/src/get-merge-request-notes.ts
+++ packages/functions/extract/src/get-merge-request-notes.ts
@@ -2,8 +2,6 @@ import type { SourceControl } from "@acme/source-control"
 import type { ExtractFunction, Entities } from "./config";
 import type { MergeRequestNote } from "@acme/extract-schema";
 import { eq } from "drizzle-orm";
-import type { LibSQLDatabase } from "drizzle-orm/libsql";
-import type { BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
 
 export type GetMergeRequestNotesInput = {
   repositoryId: number;
@@ -24,15 +22,12 @@ export const getMergeRequestNotes: GetMergeRequestNotesFunction = async (
   { repositoryId, namespaceId, mergeRequestId },
   { db, entities, integrations }
 ) => {
-  //eslint-disable-next-line @typescript-eslint/await-thenable
   const namespace = await db.select().from(entities.namespaces).where(eq(entities.namespaces.id, namespaceId)).get();
   if (!namespace) throw new Error(`Invalid namespace: ${namespaceId}`);
 
-  //eslint-disable-next-line @typescript-eslint/await-thenable
   const repository = await db.select().from(entities.repositories).where(eq(entities.repositories.id, repositoryId)).get();
   if (!repository) throw new Error(`Invalid repository: ${repositoryId}`);
 
-  //eslint-disable-next-line @typescript-eslint/await-thenable
   const mergeRequest = await db.select().from(entities.mergeRequests).where(eq(entities.mergeRequests.id, mergeRequestId)).get();
   if (!mergeRequest) throw new Error(`Invalid mergeRequest: ${mergeRequestId}`);
 
@@ -42,7 +37,7 @@ export const getMergeRequestNotes: GetMergeRequestNotesFunction = async (
 
   const { mergeRequestNotes } = await integrations.sourceControl.fetchMergeRequestNotes(repository, namespace, mergeRequest);
 
-  const insertedMergeRequestNotes = await (db as (LibSQLDatabase & BetterSQLite3Database)).transaction(async (tx) =>
+  const insertedMergeRequestNotes = await db.transaction(async (tx) =>
     Promise.all(mergeRequestNotes.map(mergeRequestNote =>
       tx.insert(entities.mergeRequestNotes).values(mergeRequestNote)
         .onConflictDoUpdate({
