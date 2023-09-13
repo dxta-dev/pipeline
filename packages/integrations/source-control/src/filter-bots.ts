27d9c249ac12c58d28815c3b4cc44bef47371dfd
Ante Koceić <ante@crocoder.dev>
2023-09-13 14:52:04 +0200

    Updated way of checking for bot keywords

--- packages/integrations/source-control/src/filter-bots.ts
+++ packages/integrations/source-control/src/filter-bots.ts
@@ -3,9 +3,17 @@ import type { GitIdentities } from "@acme/extract-schema";
 // ToDo look into more potential bots
 // GitHub bots => in name: bot / GitHub
 // GitLab bots => in email: bot
-export function filterBots(gitIdentities: Pick<GitIdentities, 'id' | 'name' | 'email'>[]) {
+const botName = ['bot', 'GitHub'];
+const botEmail = ['bot'];
+
+const checkForValue = (identity: string, valuesToCheck: string[]) => {
+  const test = valuesToCheck.filter((singleString) => identity.includes(singleString));
+  return test.length === 0;
+}
+
+export function filterBots(gitIdentities: GitIdentities[]) {
   const gitIdentitiesWithoutBots = gitIdentities.filter(
-    (identity) => !identity.name.includes('bot') && !identity.name.includes('GitHub') && !identity.email.includes('bot')
+    (identity) => checkForValue(identity.name, botName) && checkForValue(identity.email, botEmail)
   );
   return gitIdentitiesWithoutBots;
 }
