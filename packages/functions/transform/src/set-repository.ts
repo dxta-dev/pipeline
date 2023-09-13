48058777f9c1ff6ea207bae6601b84c7fb65c32a
Dejan Tot <dejan@crocoder.dev>
2023-09-13 14:42:48 +0200

    [TOOL-94] Implement Repository TransformFunction (#191)
    * transform repository function
    
    * test set-repository
    
    * add migrations
    
    * moved quesitons into review comments
    
    * change externalid of transfomed repo to be externalId of extract repo

---
+++ packages/functions/transform/src/set-repository.ts
@@ -0,0 +1,35 @@
+import { eq } from "drizzle-orm";
+import type { ExtractEntities, TransformEntities, TransformFunction } from "./config";
+import type { NewRepository as TransformedRepository } from "@acme/transform-schema";
+
+export type SetRepositoryInput = {
+  extractRepositoryId: number;
+  forgeType: 'github' | 'gitlab';
+}
+export type SetRepositoryOutput = void;
+export type SetRepositoryExtractEntities = Pick<ExtractEntities, 'repositories'>;
+export type SetRepositoryTransformEntities = Pick<TransformEntities, 'repositories'>;
+
+export type SetRepositoryFunction = TransformFunction<SetRepositoryInput, SetRepositoryOutput, SetRepositoryExtractEntities, SetRepositoryTransformEntities>;
+
+export const setRepository: SetRepositoryFunction = async (
+  { extractRepositoryId, forgeType },
+  { extract, transform }
+) => {
+
+  const extractRepository = await extract.db.select().from(extract.entities.repositories)
+    .where(eq(extract.entities.repositories.id, extractRepositoryId))
+    .get();
+
+  if (!extractRepository) throw new Error(`Repository doesn't exist: ${extractRepositoryId}`);
+
+  const transformedRepository = {
+    externalId: extractRepository.externalId,
+    forgeType,
+    name: extractRepository.name,
+  } satisfies TransformedRepository;
+
+  await transform.db.insert(transform.entities.repositories).values(transformedRepository)
+    .onConflictDoNothing()
+    .run();
+}
