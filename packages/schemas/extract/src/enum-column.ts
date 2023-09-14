84089455a83a336f14448bba197889f969fbe8d5
dejan-crocoder <dejan@crocoder.dev>
2023-09-14 10:29:16 +0200

    add enum column to extract schema

---
+++ packages/schemas/extract/src/enum-column.ts
@@ -0,0 +1,17 @@
+import { customType } from "drizzle-orm/sqlite-core";
+
+export const Enum = <U extends string, T extends Readonly<[U, ...U[]]>>(name: string, config: { enum: T }) => customType<{
+  data: T[number];
+  driverData: number;
+  config: { enum: T }
+}>({
+  dataType() {
+    return 'integer' // Sets sql column data type to integer
+  },
+  toDriver(value) {
+    return config.enum.indexOf(value);
+  },
+  fromDriver(value) {
+    return config.enum[value] as T[number];
+  }
+})(name, config);
