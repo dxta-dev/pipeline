248239af27f2552d9974ad1af3f68a73a45c49a5
dejan-crocoder <dejan@crocoder.dev>
2023-09-14 13:37:52 +0200

    unique repos idx

--- packages/schemas/extract/src/repositories.ts
+++ packages/schemas/extract/src/repositories.ts
@@ -1,17 +1,19 @@
 import type { InferSelectModel, InferInsertModel } from 'drizzle-orm';
 import { sql } from 'drizzle-orm';
-import { sqliteTable, integer,text, uniqueIndex } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, integer, text, uniqueIndex } from 'drizzle-orm/sqlite-core';
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 import { z } from 'zod';
+import { Enum } from './enum-column';
 
 export const repositories = sqliteTable('repositories', {
   id: integer('id').primaryKey(),
   externalId: integer('external_id').notNull(),
+  forgeType: Enum('forge_type', { enum: ['github', 'gitlab'] }).notNull(),
   name: text('name').notNull(),
   _createdAt: integer('__created_at', { mode: 'timestamp_ms' }).default(sql`CURRENT_TIMESTAMP`),
   _updatedAt: integer('__updated_at', { mode: 'timestamp_ms' }).default(sql`CURRENT_TIMESTAMP`),
-}, (projects) => ({
-  uniqueExternalId: uniqueIndex('repositories_external_id_idx').on(projects.externalId),
+}, (repositories) => ({
+  uniqueExternalId: uniqueIndex('repositories_external_id_idx').on(repositories.externalId, repositories.forgeType),
 }));
 
 export type Repository = InferSelectModel<typeof repositories>;
