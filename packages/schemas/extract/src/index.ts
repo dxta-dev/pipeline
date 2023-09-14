58bf70b7f12a6ba5f09109e91101737b37ef72a3
dejan-crocoder <dejan@crocoder.dev>
2023-09-14 10:28:57 +0200

    make extract schema exports more readable

--- packages/schemas/extract/src/index.ts
+++ packages/schemas/extract/src/index.ts
@@ -1,18 +1,19 @@
 export { repositories, NewRepositorySchema, RepositorySchema } from './repositories';
-export type { Repository, NewRepository } from './repositories';
 export { namespaces, NewNamespaceSchema, NamespaceSchema } from './namespaces';
-export type { Namespace, NewNamespace } from './namespaces';
 export { mergeRequests, MergeRequestSchema, NewMergeRequestSchema } from './merge-requests';
-export type { MergeRequest, NewMergeRequest } from './merge-requests';
 export { mergeRequestDiffs } from './merge-request-diffs';
-export type { MergeRequestDiff, NewMergeRequestDiff } from './merge-request-diffs';
 export { mergeRequestCommits } from './merge-request-commits';
-export type { MergeRequestCommit, NewMergeRequestCommit } from './merge-request-commits';
 export { members, MemberSchema, NewMemberSchema } from './members';
-export type { Member, NewMember } from './members';
 export { repositoriesToMembers } from './repositories-to-members';
-export type { RepositoryToMember, NewRepositoryToMember } from './repositories-to-members';
-export { mergeRequestNotes } from './merge-request-notes';
-export type { MergeRequestNote, NewMergeRequestNote } from './merge-request-notes';
+export { mergeRequestNotes} from './merge-request-notes';
 export { gitIdentities } from './git-identities';
+
+export type { Repository, NewRepository } from './repositories';
+export type { Namespace, NewNamespace } from './namespaces';
+export type { MergeRequest, NewMergeRequest } from './merge-requests';
+export type { MergeRequestDiff, NewMergeRequestDiff } from './merge-request-diffs';
+export type { MergeRequestCommit, NewMergeRequestCommit } from './merge-request-commits';
+export type { Member, NewMember } from './members';
+export type { RepositoryToMember, NewRepositoryToMember } from './repositories-to-members';
+export type { MergeRequestNote, NewMergeRequestNote } from './merge-request-notes';
 export type { GitIdentities, NewGitIdentities } from './git-identities';
