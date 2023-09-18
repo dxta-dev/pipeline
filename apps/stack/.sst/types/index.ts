98e2db3be960400844ff894460ed162e9567f84d
dejan-crocoder <dejan@crocoder.dev>
2023-09-18 15:18:50 +0200

    fix gitignore

---
+++ apps/stack/.sst/types/index.ts
@@ -0,0 +1,70 @@
+import "sst/node/config";
+declare module "sst/node/config" {
+  export interface ConfigTypes {
+    APP: string;
+    STAGE: string;
+  }
+}import "sst/node/config";
+declare module "sst/node/config" {
+  export interface SecretResources {
+    "DATABASE_URL": {
+      value: string;
+    }
+  }
+}import "sst/node/config";
+declare module "sst/node/config" {
+  export interface SecretResources {
+    "DATABASE_AUTH_TOKEN": {
+      value: string;
+    }
+  }
+}import "sst/node/config";
+declare module "sst/node/config" {
+  export interface SecretResources {
+    "CLERK_SECRET_KEY": {
+      value: string;
+    }
+  }
+}import "sst/node/config";
+declare module "sst/node/config" {
+  export interface SecretResources {
+    "REDIS_URL": {
+      value: string;
+    }
+  }
+}import "sst/node/config";
+declare module "sst/node/config" {
+  export interface SecretResources {
+    "REDIS_TOKEN": {
+      value: string;
+    }
+  }
+}import "sst/node/config";
+declare module "sst/node/config" {
+  export interface ParameterResources {
+    "REDIS_USER_TOKEN_TTL": {
+      value: string;
+    }
+  }
+}import "sst/node/event-bus";
+declare module "sst/node/event-bus" {
+  export interface EventBusResources {
+    "ExtractBus": {
+      eventBusName: string;
+    }
+  }
+}import "sst/node/queue";
+declare module "sst/node/queue" {
+  export interface QueueResources {
+    "ExtractQueue": {
+      queueUrl: string;
+    }
+  }
+}import "sst/node/api";
+declare module "sst/node/api" {
+  export interface ApiResources {
+    "ExtractApi": {
+      url: string;
+    }
+  }
+}
