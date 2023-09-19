744d68a93c727942f93fb9d6a5676a3a298709f2
dejan-crocoder <dejan@crocoder.dev>
2023-09-19 09:38:19 +0200

    implement transform repository

--- apps/stack/src/transform/transform-repository.ts
+++ apps/stack/src/transform/transform-repository.ts
@@ -5,17 +5,26 @@ import { Config } from "sst/node/config";
 import { EventHandler } from "sst/node/event-bus";
 
 import { extractRepositoryEvent } from "@stack/extract/events";
+import * as extract from "@acme/extract-schema";
+import * as transform from "@acme/transform-schema";
 import type { Context, SetRepositoryExtractEntities, SetRepositoryTransformEntities } from "@acme/transform-functions";
+import { setRepository } from "@acme/transform-functions";
 
-// const context: Context<SetRepositoryExtractEntities, SetRepositoryTransformEntities> = {
-//   extract: {
-// db: drizzle(createClient({ url: Config.DATABASE_URL, authToken: Config.DATABASE_AUTH_TOKEN }))
-//   },
-//   transform: {
-// db:drizzle(createClient({url:Config.}))
-//   }
-// }
+const context: Context<SetRepositoryExtractEntities, SetRepositoryTransformEntities> = {
+  extract: {
+    db: drizzle(createClient({ url: Config.DATABASE_URL, authToken: Config.DATABASE_AUTH_TOKEN })),
+    entities: {
+      repositories: extract.repositories
+    }
+  },
+  transform: {
+    db: drizzle(createClient({ url: Config.TRANSFORM_DB_URL, authToken: Config.TRANSFORM_DB_TOKEN })),
+    entities: {
+      repositories: transform.repositories
+    }
+  }
+}
 
 export const eventHandler = EventHandler(extractRepositoryEvent, async (evt) => {
-  console.log("it is alive !!!");
+  await setRepository({ extractRepositoryId: evt.properties.repositoryId }, context);
 });
