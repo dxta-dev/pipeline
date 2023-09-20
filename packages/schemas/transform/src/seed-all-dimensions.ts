88d993dd0c284cdbbe865dbbb2dbad5547bbf83a
ivke995 <ivanivic842@gmail.com>
2023-09-20 14:52:21 +0200

    logic for calculating calendar like time fix

--- packages/schemas/transform/src/seed-all-dimensions.ts
+++ packages/schemas/transform/src/seed-all-dimensions.ts
@@ -1,7 +1,40 @@
 
+const currentYear = new Date().getFullYear();
+    const yearsInPast = 3;
+    const yearsInFuture = 3;
+    const pastStartYear = currentYear - yearsInPast;
+    const futureEndYear = currentYear + yearsInFuture;
 
-async function seedDates() {
-    try {
-        const drizzle = new Drizzle()
+    let weekNumber = 1; 
+    let yearSeed = 0; 
+    let dayOfWeek = 0; 
+
+    for (let year = pastStartYear; year <= futureEndYear; year++) {
+      if (year !== yearSeed) {
+        weekNumber = 1;
+        yearSeed = year;
+      }
+
+      for (let month = 1; month <= 12; month++) {
+        let daysInMonth = 31; 
+
+        if (month === 4 || month === 6 || month === 9 || month === 11) {
+          daysInMonth = 30;
+        } else if (month === 2) {
+          daysInMonth = year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0) ? 29 : 28;
+        }
+
+        for (let day = 1; day <= daysInMonth; day++) {
+          console.log(weekNumber)
+
+
+          if (dayOfWeek === 6) {
+            weekNumber++;
+          }
+
+          dayOfWeek = (dayOfWeek + 1) % 7;
+        }
+      }
     }
-}
+
+
