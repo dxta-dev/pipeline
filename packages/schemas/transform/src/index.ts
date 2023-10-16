22e0579c1120b642809c868810d768a8d0f4194c
David Abram <david@crocoder.dev>
2023-10-16 15:53:16 +0200

    changes to tables

--- packages/schemas/transform/src/index.ts
+++ packages/schemas/transform/src/index.ts
@@ -2,10 +2,14 @@ export { forgeUsers } from './forge-users';
 export { mergeRequests } from './merge-requests';
 export { repositories } from './repositories';
 export { dates } from './dates';
-export { mergeRequestFacts } from './merge-request-facts'
+export { mergeRequestMetrics } from './merge-request-metrics';
+export { mergeRequestUsersJunk } from './merge-request-users-junk';
+export { mergeRequestDatesJunk } from './merge-request-dates-junk';
 
 export type { ForgeUser, NewForgeUser } from './forge-users';
 export type { MergeRequest, NewMergeRequest } from './merge-requests';
 export type { Repository, NewRepository } from './repositories';
 export type { TransformDate, NewTransformDate } from './dates';
-export type { MergeRequestFact, NewMergeRequestFact } from './merge-request-facts';
+export type { MergeRequestMetric, NewMergeRequestMetric } from './merge-request-metrics';
+export type { MergeRequestUsersJunk, NewMergeRequestUsersJunk } from './merge-request-users-junk';
+export type { MergeRequestDatesJunk, NewMergeRequestDatesJunk } from './merge-request-dates-junk';
