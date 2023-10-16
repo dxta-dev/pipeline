269be8441e9922a16c4980be265b76fd57494e2d
David Abram <david@crocoder.dev>
2023-10-16 12:10:19 +0200

    Users have junk in their trunk (#271)
    * Users have junk in their trunk
    
    * fix index.ts
    
    * que

--- packages/schemas/transform/src/index.ts
+++ packages/schemas/transform/src/index.ts
@@ -2,10 +2,10 @@ export { forgeUsers } from './forge-users';
 export { mergeRequests } from './merge-requests';
 export { repositories } from './repositories';
 export { dates } from './dates';
-export { mergeRequestReports } from './merge-request-reports'
+export { mergeRequestFacts } from './merge-request-facts'
 
 export type { ForgeUser, NewForgeUser } from './forge-users';
 export type { MergeRequest, NewMergeRequest } from './merge-requests';
 export type { Repository, NewRepository } from './repositories';
 export type { TransformDate, NewTransformDate } from './dates';
-export type { MergeRequestReport, NewMergeRequestReport } from './merge-request-reports';
+export type { MergeRequestFact, NewMergeRequestFact } from './merge-request-facts';
--- packages/schemas/transform/src/merge-request-facts.ts
+++ packages/schemas/transform/src/merge-request-facts.ts
@@ -2,14 +2,14 @@ import type { InferSelectModel, InferInsertModel } from 'drizzle-orm';
 import { sql } from 'drizzle-orm';
 import { sqliteTable, integer } from 'drizzle-orm/sqlite-core';
 import { repositories } from './repositories';
-import { forgeUsers } from './forge-users';
 import { mergeRequests } from './merge-requests';
 import { dates } from './dates';
+import { userJunk } from './users-junk';
 
-export const mergeRequestReports = sqliteTable('merge_request_reports', {
+export const mergeRequestFacts = sqliteTable('merge_request_facts', {
   id: integer('id').primaryKey(),
   // Dimensions
-  forgeUserId: integer('forge_user_id').notNull().references(() => forgeUsers.id),
+  userJunk: integer('user_junk_id').notNull().references(() => userJunk.id),
   repositoryId: integer('repository_id').notNull().references(() => repositories.id),
   mergeRequestId: integer('merge_request_id').notNull().references(() => mergeRequests.id),
   dateId: integer('date_id').notNull().references(() => dates.id),
@@ -22,9 +22,9 @@ export const mergeRequestReports = sqliteTable('merge_request_reports', {
   approved: integer('approved', { mode: 'boolean' }).default(false),
   reviewDepth: integer('review_depth').notNull(),
   // Meta
-  _createdAt: integer('__created_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
-  _updatedAt: integer('__updated_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
+  _createdAt: integer('__created_at', { mode: 'timestamp_ms' }).default(sql`CURRENT_TIMESTAMP`),
+  _updatedAt: integer('__updated_at', { mode: 'timestamp_ms' }).default(sql`CURRENT_TIMESTAMP`),
 });
 
-export type MergeRequestReport = InferSelectModel<typeof mergeRequestReports>;
-export type NewMergeRequestReport = InferInsertModel<typeof mergeRequestReports>;
+export type MergeRequestFact = InferSelectModel<typeof mergeRequestFacts>;
+export type NewMergeRequestFact = InferInsertModel<typeof mergeRequestFacts>;
