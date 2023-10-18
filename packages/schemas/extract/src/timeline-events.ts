5c4d4d316cf89974ebf1e8a9c81592f615fe22ff
Ante Koceić <ante@crocoder.dev>
2023-10-18 16:56:19 +0200

    Updated timelineEvents

--- packages/schemas/extract/src/timeline-events.ts
+++ packages/schemas/extract/src/timeline-events.ts
@@ -11,14 +11,25 @@ export const timelineEvents = sqliteTable('timeline_events', {
       enum:
         [
           'assigned',
+          'closed',
+          'commented',
+          'committed',
+          'convert_to_draft',
+          'deployed',
+          'head_ref_force_pushed',
+          'merged',
+          'ready_for_review',
+          'review_request_removed',
+          'review_requested',
+          'reviewed',
+          'unassigned',
         ]
     }
   ).notNull(),
   mergeRequestId: integer('merge_request_id').notNull(),
-  eventType: text('event_type').notNull(),
   timestamp: integer('timestamp').notNull(),
-  actorId: integer('actor_id').notNull(),
   actorName: text('actor_name').notNull(),
+  actorId: integer('actor_id'),
   actorEmail: text('actor_email'),
   data: text('data', { mode: 'json' }).notNull(),
   _createdAt: integer('__created_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
@@ -33,7 +44,39 @@ export const AssignedEventSchema = z.object({
   }),
 });
 
-export type AssingedEvent = z.infer<typeof AssignedEventSchema>;
+export const CommittedEventSchema = z.object({
+  committer: z.object({
+    email: z.number(),
+    name: z.string(),
+  }),
+  message: z.string(),
+});
+
+export const CommentedEventEventSchema = z.object({
+  body: z.string(),
+});
+
+export const ReviewRequestedEventSchema = z.object({
+  requestedReviewere: z.object({
+    id: z.number(),
+    name: z.string(),
+  }),
+});
+
+export const ReviewSchemaEventSchema = z.object({
+  review: z.object({
+    id: z.number(),
+    name: z.string(),
+  }),
+  body: z.string(),
+  state: z.string(),
+});
+
+export type AssignedEvent = z.infer<typeof AssignedEventSchema>;
+export type CommittedEvent = z.infer<typeof CommittedEventSchema>;
+export type CommentedEventEvent = z.infer<typeof CommentedEventEventSchema>;
+export type ReviewRequestedEvent = z.infer<typeof ReviewRequestedEventSchema>;
+export type ReviewSchemaEvent = z.infer<typeof ReviewSchemaEventSchema>;
 
 export type TimelineEvents = InferSelectModel<typeof timelineEvents>;
 export type NewTimelineEvents = InferInsertModel<typeof timelineEvents>;
