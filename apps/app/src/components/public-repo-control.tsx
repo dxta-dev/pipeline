9529bc19f168d83720845a8c8398a14976ca0573
dejan-crocoder <dejan@crocoder.dev>
2023-12-08 17:03:17 +0100

    move ui things ? idk

--- apps/app/src/components/public-repo-control.tsx
+++ apps/app/src/components/public-repo-control.tsx
@@ -59,10 +59,10 @@ const PublicRepoCard = ({ repo }: PublicRepoCardProps) => {
         <option value="360">360 days</option>
       </select>
       
-      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Status: {status}</p>
       <button onClick={() => { onExtractClick().catch(console.log) }} type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Extract</button>
       {/* NO PERIOD FOR TRANSFORM EXISTS, OR PER REPOSITORY */}
       {/* <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Transform</button> */}
+      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Status: {status}</p>
     </div>
   )
 }
