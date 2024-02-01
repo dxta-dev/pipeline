33368df1fe6ede81abaad55a831908ab0de20a0c
dejan-crocoder <dejan@crocoder.dev>
2024-02-01 15:02:09 +0100

    removed next

--- apps/app-htmx/src/app-config.ts
+++ apps/app-htmx/src/app-config.ts
@@ -5,9 +5,8 @@ const ENVSchema = z.object({
   SUPER_DATABASE_AUTH_TOKEN: z.string().optional(),
   TENANT_DATABASE_AUTH_TOKEN: z.string().optional(),
 
-  // TODO: remove after removing next
-  NEXT_PUBLIC_EXTRACT_API_URL: z.string(),
-  NEXT_PUBLIC_TRANSFORM_API_URL: z.string(),
+  PUBLIC_EXTRACT_API_URL: z.string(),
+  PUBLIC_TRANSFORM_API_URL: z.string(),
 
   CLERK_PUBLISHABLE_KEY: z.string(),
   CLERK_SECRET_KEY: z.string(),
@@ -26,8 +25,8 @@ export const AppConfig = {
   },
   tenantDatabaseAuthToken: parsedEnv.data.TENANT_DATABASE_AUTH_TOKEN,
   apis: {
-    extractStart: parsedEnv.data.NEXT_PUBLIC_EXTRACT_API_URL,
-    transformStart: parsedEnv.data.NEXT_PUBLIC_TRANSFORM_API_URL,
+    extractStart: parsedEnv.data.PUBLIC_EXTRACT_API_URL,
+    transformStart: parsedEnv.data.PUBLIC_TRANSFORM_API_URL,
   },
   clerk: {
     domain: parsedEnv.data.CLERK_DOMAIN,
