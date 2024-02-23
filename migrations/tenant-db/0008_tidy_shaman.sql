ee8d635a88bf36550883c98cce3ceefea93f145f
ivke995 <ivanivic842@gmail.com>
2024-02-23 14:24:23 +0100

    resolve conflicts actions

--- migrations/tenant-db/0008_tidy_shaman.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE extract_members ADD `profile_url` text DEFAULT '';--> statement-breakpoint
-ALTER TABLE extract_members ADD `avatar_url` text DEFAULT '';
--- migrations/tenant-db/0009_little_groot.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE transform_forge_users ADD `profile_url` text DEFAULT '';--> statement-breakpoint
-ALTER TABLE transform_forge_users ADD `avatar_url` text DEFAULT '';
