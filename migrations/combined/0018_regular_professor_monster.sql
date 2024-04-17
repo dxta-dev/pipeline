d60c9d7774f67982b4db2350187e355496a45121
ivke995 <ivanivic842@gmail.com>
2024-04-17 09:33:10 +0200

    fix column name and target table

--- migrations/combined/0018_regular_professor_monster.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE extract_merge_request_commits ADD `commit_url` text DEFAULT '' NOT NULL;--> statement-breakpoint
-ALTER TABLE transform_merge_request_events ADD `commit_url` text DEFAULT '' NOT NULL;
