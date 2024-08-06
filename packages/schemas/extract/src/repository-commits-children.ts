34d15abdeb12f03bbe9eb82bb4a8b5ce46d0eb77
dejan-crocoder <dejan@crocoder.dev>
2024-08-06 13:58:40 +0200

    autoincrement key to composite key in many to many table

--- packages/schemas/extract/src/repository-commits-children.ts
+++ packages/schemas/extract/src/repository-commits-children.ts
@@ -1,19 +1,18 @@
 import type { InferSelectModel, InferInsertModel } from 'drizzle-orm';
 import { sql } from 'drizzle-orm';
-import { integer, uniqueIndex } from 'drizzle-orm/sqlite-core';
+import { integer, primaryKey } from 'drizzle-orm/sqlite-core';
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 import { z } from 'zod';
 import { sqliteTable } from './extract-table';
 import { repositoryCommits } from './repository-commits';
 
 export const repositoryCommitsChildren = sqliteTable('repository_commits_children', {
-  id: integer('id').primaryKey(),
   commit: integer('commit').references(() => repositoryCommits.id).notNull(),
   childOf: integer('parent').references(() => repositoryCommits.id).notNull(),
   _createdAt: integer('__created_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
   _updatedAt: integer('__updated_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
 }, (commitsChildren) => ({
-  uniqueCommitChild: uniqueIndex('repository_commits_children_idx').on(commitsChildren.commit, commitsChildren.childOf)
+  pk: primaryKey({ columns: [commitsChildren.commit, commitsChildren.childOf] }),
 }));
 
 export type CommitChild = InferSelectModel<typeof repositoryCommitsChildren>;
