0d63c430d859a2647dc1cc829473ee0f345e6b5e
dejan-crocoder <dejan@crocoder.dev>
2024-08-23 13:35:34 +0200

    modify commit schema

--- packages/schemas/extract/src/repository-commits.ts
+++ packages/schemas/extract/src/repository-commits.ts
@@ -1,6 +1,6 @@
 import type { InferSelectModel, InferInsertModel } from 'drizzle-orm';
 import { sql } from 'drizzle-orm';
-import { integer, uniqueIndex } from 'drizzle-orm/sqlite-core';
+import { integer, text, uniqueIndex } from 'drizzle-orm/sqlite-core';
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 import { z } from 'zod';
 import { repositories } from './repositories';
@@ -9,11 +9,7 @@ import { sqliteTable } from './extract-table';
 export const repositoryCommits = sqliteTable('repository_commits', {
   id: integer('id').primaryKey(),
   repositoryId: integer('repository_id').references(() => repositories.id).notNull(),
-  sha0: integer('sha_0').notNull(),
-  sha1: integer('sha_1').notNull(),
-  sha2: integer('sha_2').notNull(),
-  sha3: integer('sha_3').notNull(),
-  sha4: integer('sha_4').notNull(),
+  sha: text('sha').notNull(),
   committedAt: integer('committed_at', { mode: 'timestamp_ms' }),
   authoredAt: integer('authored_at', { mode: 'timestamp_ms' }),
   _createdAt: integer('__created_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
@@ -21,11 +17,7 @@ export const repositoryCommits = sqliteTable('repository_commits', {
 }, (commits) => ({
   uniqueRepositorySha: uniqueIndex('repository_commits_repository_sha_idx').on(
     commits.repositoryId,
-    commits.sha0,
-    commits.sha1,
-    commits.sha2,
-    commits.sha3,
-    commits.sha4,
+    commits.sha,
   ),
 }),
 );
@@ -45,20 +37,3 @@ export const NewCommitSchema = createInsertSchema(repositoryCommits, {
   _updatedAt: z.coerce.date(),
 });
 
-export const marshalSha = (sha: string) => {
-  return {
-    sha0: parseInt(sha.slice(0 * 8, 1 * 8), 16),
-    sha1: parseInt(sha.slice(1 * 8, 2 * 8), 16),
-    sha2: parseInt(sha.slice(2 * 8, 3 * 8), 16),
-    sha3: parseInt(sha.slice(3 * 8, 4 * 8), 16),
-    sha4: parseInt(sha.slice(4 * 8, 5 * 8), 16),
-  }
-};
-
-export const unmarshalSha = (commit: Commit) => {
-  return commit.sha0.toString(16).padStart(8, "0")
-    + commit.sha1.toString(16).padStart(8, "0")
-    + commit.sha2.toString(16).padStart(8, "0")
-    + commit.sha3.toString(16).padStart(8, "0")
-    + commit.sha4.toString(16).padStart(8, "0");
-};
