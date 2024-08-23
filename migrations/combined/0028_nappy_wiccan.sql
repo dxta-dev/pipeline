6ae823a6c2f414d69930ae27210a6260a7497f37
David Abram <david@crocoder.dev>
2024-08-23 17:06:04 +0200

    migrations!

---
+++ migrations/combined/0028_nappy_wiccan.sql
@@ -0,0 +1,1 @@
+ALTER TABLE extract_namespaces ADD `namespace_type` integer DEFAULT 0 NOT NULL;
--- packages/schemas/extract/src/namespaces.ts
+++ packages/schemas/extract/src/namespaces.ts
@@ -10,7 +10,7 @@ export const namespaces = sqliteTable('namespaces', {
   id: integer('id').primaryKey(),
   externalId: integer('external_id').notNull(),
   forgeType: Enum('forge_type', { enum: ['github', 'gitlab'] }).notNull(),
-  namespaceType: Enum('namespace_type', { enum: ['organization', 'personal'] }).notNull().default('organization'),
+  namespaceType: Enum('namespace_type', { enum: ['organization', 'personal'] }).notNull().default((0 as any)),
   name: text('name').notNull(),
   _createdAt: integer('__created_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
   _updatedAt: integer('__updated_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
@@ -24,9 +24,11 @@ export const NewNamespaceSchema = createInsertSchema(namespaces, {
   _createdAt: z.coerce.date(),
   _updatedAt: z.coerce.date(),
   forgeType: z.literal('github').or(z.literal('gitlab')),
+  namespaceType: z.literal('organization').or(z.literal('personal'))
 });
 export const NamespaceSchema = createSelectSchema(namespaces, {
   _createdAt: z.coerce.date(),
   _updatedAt: z.coerce.date(),
   forgeType: z.literal('github').or(z.literal('gitlab')),
+  namespaceType: z.literal('organization').or(z.literal('personal'))
 });
