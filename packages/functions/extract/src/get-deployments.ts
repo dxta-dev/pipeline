a2be816ca788b7b5b5bd965b4c3e244666faf56c
dejan-crocoder <dejan@crocoder.dev>
2024-08-26 13:49:15 +0200

    update get-deployments

--- packages/functions/extract/src/get-deployments.ts
+++ packages/functions/extract/src/get-deployments.ts
@@ -1,5 +1,4 @@
-import type { Deployment, Namespace, Repository, NewCommit, Commit } from "@dxta/extract-schema";
-import { marshalSha, unmarshalSha } from "@dxta/extract-schema";
+import type { Deployment, Namespace, Repository, Sha } from "@dxta/extract-schema";
 import type { ExtractFunction, Entities } from "./config";
 import type { Pagination, SourceControl } from "@dxta/source-control";
 import { sql } from "drizzle-orm";
@@ -18,7 +17,7 @@ export type GetDeploymentsOutput = {
 };
 
 export type GetDeploymentsSourceControl = Pick<SourceControl, "fetchDeployments">;
-export type GetDeploymentsEntities = Pick<Entities, "deployments" | "commits">;
+export type GetDeploymentsEntities = Pick<Entities, "deployments" | "repositoryShas">;
 
 export type GetDeploymentsFunction = ExtractFunction<GetDeploymentsInput, GetDeploymentsOutput, GetDeploymentsSourceControl, GetDeploymentsEntities>;
 
@@ -33,34 +32,21 @@ export const getDeployments: GetDeploymentsFunction = async (
 
   const { deployments, pagination } = await integrations.sourceControl.fetchDeployments(repository, namespace, perPage, environment, page);
 
-  const deploymentCommits = deployments.map(x => ({
-    repositoryId: repository.id,
-    ...marshalSha(x.commitSha)
-  } satisfies NewCommit));
-
-  const insertedCommits = await db.transaction(async (tx) => {
+  const insertedShas = await db.transaction(async (tx)=> {
     return Promise.all(
-      deploymentCommits.map(commit =>
-        tx.insert(entities.commits).values(commit)
+      deployments.map(deployment=>
+        tx.insert(entities.repositoryShas).values({ repositoryId: repository.id, sha: deployment.commitSha })
           .onConflictDoUpdate({
-            target: [
-              entities.commits.repositoryId,
-              entities.commits.sha0,
-              entities.commits.sha1,
-              entities.commits.sha2,
-              entities.commits.sha3,
-              entities.commits.sha4,
-            ],
-            set: {
-              _updatedAt: sql`(strftime('%s', 'now'))`,
-            }
+            target: [entities.repositoryShas.repositoryId, entities.repositoryShas.sha],
+            set: { _updatedAt: sql`(strftime('%s', 'now'))` },
           })
           .returning()
           .get()
       )
     );
   });
-  const commitShaIdMap = insertedCommits.reduce((map, commit) => map.set(unmarshalSha(commit), commit.id), new Map<string, Commit['id']>())
+
+  const shaIdMap = insertedShas.reduce((map, sha) => map.set(sha.sha, sha.id), new Map<string, Sha['id']>());
 
   const insertedDeployments = await db.transaction(async (tx) => {
     return Promise.all(deployments.map(deployment =>
@@ -69,7 +55,8 @@ export const getDeployments: GetDeploymentsFunction = async (
         repositoryId: deployment.repositoryId,
         environment: deployment.environment,
         status: deployment.status,
-        commitId: commitShaIdMap.get(deployment.commitSha) as number,
+        repositoryShaId: shaIdMap.get(deployment.commitSha) as number,
+        deploymentType: 'github-deployment',        
         createdAt: deployment.createdAt,
         updatedAt: deployment.updatedAt,
         deployedAt: deployment.deployedAt,
@@ -82,7 +69,6 @@ export const getDeployments: GetDeploymentsFunction = async (
           set: {
             updatedAt: deployment.updatedAt,
             environment: deployment.environment,
-            commitId: commitShaIdMap.get(deployment.commitSha),
             createdAt: deployment.createdAt,
             _updatedAt: sql`(strftime('%s', 'now'))`,
           },
