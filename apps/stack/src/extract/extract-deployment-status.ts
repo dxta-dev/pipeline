bc84cf67e9439ed0ef641e94e8dc3e50899096b5
dejan-crocoder <dejan@crocoder.dev>
2024-10-01 13:47:48 +0200

    refactor: update extract-deployment-status

--- apps/stack/src/extract/extract-deployment-status.ts
+++ apps/stack/src/extract/extract-deployment-status.ts
@@ -6,10 +6,10 @@ import { z } from "zod"
 import type { Context, GetDeploymentStatusEntities, GetDeploymentStatusSourceControl } from "@dxta/extract-functions"
 import { getDeploymentStatus } from "@dxta/extract-functions";
 import { MessageKind, metadataSchema } from "./messages"
-import { getClerkUserToken } from "./get-clerk-user-token"
-import { GitHubSourceControl, GitlabSourceControl } from "@dxta/source-control"
-import { getTenantDb, type OmitDb } from "@stack/config/get-tenant-db"
 import { and, eq, inArray, isNull, or } from "drizzle-orm"
+import { initDatabase, initIntegrations } from "./context"
+
+type ExtractDeploymentStatusContext = Context<GetDeploymentStatusSourceControl, GetDeploymentStatusEntities>;
 
 export const deploymentStatusSenderHandler = createMessageHandler({
   queueId: 'ExtractQueue',
@@ -22,19 +22,22 @@ export const deploymentStatusSenderHandler = createMessageHandler({
   }).shape,
   handler: async (message) => {
     const { repository, namespace, deployment } = message.content;
-    const { tenantId, userId, sourceControl } = message.metadata;
 
     if (deployment.status !== null && deployment.status !== 'pending') {
       console.log("Skipping extract-deployment-status for status:", deployment.status);
       return;
     }
 
-    context.integrations.sourceControl = await initSourceControl(userId, sourceControl);
+    const dynamicContext = {
+      integrations: await initIntegrations(message.metadata),
+      db: initDatabase(message.metadata),
+    } satisfies Partial<ExtractDeploymentStatusContext>;
+
     await getDeploymentStatus({
       namespace,
       repository,
       deployment
-    }, { ...context, db: getTenantDb(tenantId) });
+    }, { ...staticContext, ...dynamicContext });
 
   }
 });
@@ -42,8 +45,8 @@ export const deploymentStatusSenderHandler = createMessageHandler({
 const { sender } = deploymentStatusSenderHandler;
 
 export const onExtractRepository = EventHandler(extractRepositoryEvent, async (ev) => {
-  const { crawlId, from, to, userId, sourceControl, tenantId } = ev.metadata;
-  const db = getTenantDb(tenantId);
+  const { crawlId, from, to, userId, sourceControl, dbUrl } = ev.metadata;
+  const db = initDatabase(ev.metadata);
 
   const repository = await db.select().from(repositories).where(eq(repositories.id, ev.properties.repositoryId)).get();
   const namespace = await db.select().from(namespaces).where(eq(namespaces.id, ev.properties.namespaceId)).get();
@@ -81,7 +84,7 @@ export const onExtractRepository = EventHandler(extractRepositoryEvent, async (e
     from,
     to,
     crawlId,
-    tenantId,
+    dbUrl,
   });
 
 }, {
@@ -89,9 +92,9 @@ export const onExtractRepository = EventHandler(extractRepositoryEvent, async (e
 });
 
 export const onExtractDeployments = EventHandler(extractDeploymentsEvent, async (ev) => {
-  const { crawlId, from, to, userId, sourceControl, tenantId } = ev.metadata;
-  const db = getTenantDb(tenantId);
-
+  const { crawlId, from, to, userId, sourceControl, dbUrl } = ev.metadata;
+  
+  const db = initDatabase(ev.metadata);
   const repository = await db.select().from(repositories).where(eq(repositories.id, ev.properties.repositoryId)).get();
   const namespace = await db.select().from(namespaces).where(eq(namespaces.id, ev.properties.namespaceId)).get();
   const newDeployments = await db.select().from(deployments).where(inArray(deployments.id, ev.properties.deploymentIds)).all();
@@ -117,24 +120,13 @@ export const onExtractDeployments = EventHandler(extractDeploymentsEvent, async 
     from,
     to,
     crawlId,
-    tenantId,
+    dbUrl,
   });
 
 });
 
-
-const initSourceControl = async (userId: string, sourceControl: 'github' | 'gitlab') => {
-  const accessToken = await getClerkUserToken(userId, `oauth_${sourceControl}`);
-  if (sourceControl === 'github') return new GitHubSourceControl({ auth: accessToken });
-  if (sourceControl === 'gitlab') return new GitlabSourceControl(accessToken);
-  return null;
-}
-
-const context: OmitDb<Context<GetDeploymentStatusSourceControl, GetDeploymentStatusEntities>> = {
+const staticContext = {
   entities: {
     deployments
   },
-  integrations: {
-    sourceControl: null,
-  },
-};
+} satisfies Partial<ExtractDeploymentStatusContext>;
