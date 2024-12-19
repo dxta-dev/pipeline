83792d1328b7f243c5e0057cb2881ac2b5369bd7
Dejan Tot <dejan@crocoder.dev>
2024-12-19 15:25:54 +0100

    feat: github error rethrowing (#491)

---
+++ packages/integrations/source-control/src/github/errors.ts
@@ -0,0 +1,15 @@
+import type { RateLimitState } from "./rate-limits";
+
+export class RateLimitExceededError extends Error {
+
+  name: "RateLimitExceededError";
+  state: RateLimitState;
+
+  constructor(message: string, state: RateLimitState) {
+    super(message);
+
+    this.name = "RateLimitExceededError";
+    this.state = state;
+  }
+
+}
