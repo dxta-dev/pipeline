98df50fca20f6a59a84afb40773be3b7ada6c6b1
dejan-crocoder <dejan@crocoder.dev>
2025-02-18 11:35:40 +0100

    simple memory caching.. fingers-crossed

--- apps/stack/src/extract/get-clerk-user-token.ts
+++ apps/stack/src/extract/get-clerk-user-token.ts
@@ -5,6 +5,11 @@ type SupportedClerkOAuthProviders = 'oauth_github' | 'oauth_gitlab';
 
 const clerkClient = Clerk({ secretKey: Config.CLERK_SECRET_KEY });
 
+// issue: never evicted so needs to be revised when have more than 100 unique tokens
+// Use warm lambda to cache tokens in memory
+const tokens = new Map<string, string>();
+const expirations = new Map<string, number>();
+
 const fetchClerkUserToken = async (userId: string, provider: SupportedClerkOAuthProviders) => {
   const [userOauthAccessTokenPayload] = await clerkClient.users.getUserOauthAccessToken(userId, provider);
 
@@ -15,9 +20,20 @@ const fetchClerkUserToken = async (userId: string, provider: SupportedClerkOAuth
   return userOauthAccessTokenPayload.token;
 };
 
-export const getClerkUserToken = async (userId: string, provider: SupportedClerkOAuthProviders) => {
+export const getClerkUserToken = async (userId: string, provider: SupportedClerkOAuthProviders, ts: number = Date.now()) => {
+  const cacheKey = `${userId}_${provider}`;
+  
+  const exp = expirations.get(cacheKey);
+  const cachedUserToken = tokens.get(cacheKey);
+  const isCached = exp !== undefined && ts < exp;
 
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  if (isCached) return cachedUserToken!;
+  
   const userToken = await fetchClerkUserToken(userId, provider);
+
+  expirations.set(cacheKey, ts + Number(Config.REDIS_USER_TOKEN_TTL) * 1000)
+  tokens.set(cacheKey, userToken);
 
   return userToken;
 }
